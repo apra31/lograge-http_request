f01e5104bdf1a9eaa719afdd79707189e383e4e6
Aditya Pradiptadarma <a.pra31@gmail.com>
2019-12-10 22:43:46 +0700

    Handle various response_body encoding.

--- lib/adapters/http_log.rb
+++ lib/adapters/http_log.rb
@@ -8,6 +8,17 @@ module HttpLog
 
     def call(options = {})
       orig_call(options)
+      lograge_http_requests = options.dup
+      lograge_http_requests[:response_body] =
+        begin
+          HttpLog.parse_body(
+            options[:response_body],
+            options[:encoding],
+            options[:content_type]
+          )
+        rescue BodyParsingError => e
+          "Response: #{e.message}"
+        end
 
       Lograge::HttpRequest.store[:lograge_http_requests] ||= []
       Lograge::HttpRequest.store[:lograge_http_requests] << options
